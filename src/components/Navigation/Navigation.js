b64c2fc615350b3f913cae5d3811f83fe3e336e0
timothyrusso <russotimothy@live.it>
2022-04-29 06:56:06 +0200

    feat: navigation links styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,13 @@
 import './Navigation.css';
+import logo from '../../images/NewsExplorer.svg'
 
 const Navigation = () => {
   return (
     <div className="navigation">
-      <img src='' alt='' className='logo'></img>
+      <img src={logo} alt='' className='logo'></img>
       <div className='navigation__wrapper'>
-        <a href='' className='navigation__link'>test</a>
-        <button aria-label="signin" type="button" className='navigation__button'>test</button>
+        <a href='' className='navigation__link'>Home</a>
+        <button aria-label="signin" type="button" className='navigation__button'>Sign in</button>
       </div>
     </div>
   );
