e552712981e3c26f7a672e4495032b8a08512372
timothyrusso <russotimothy@live.it>
2022-04-29 08:34:27 +0200

    feat: searchform styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import './Header.css';
+import SearchForm from '../SearchForm/SearchForm';
 
-const Header = ({ children }) => {
+const Header = () => {
 
   return (
     <header className='header'>
       <h1 className='header__title'>What's going on in the world?</h1>
       <p className='header__subtitle'>Find the latest news on any topic and save them in your personal account.</p>
-      {children}
+      <SearchForm />
     </header>
   );
 }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,6 +3,8 @@ import './SearchForm.css';
 const SearchForm = () => {
   return (
     <form className='search-form'>
+        <input type="text" className="search-form__input" id="search-input" name="search" placeholder="Enter topic"></input>
+        <button className="search-form__button">Search</button>
     </form>
   );
 }
