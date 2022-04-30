406677f3c97dde3bbf45711cf8f11255be0fda79
timothyrusso <russotimothy@live.it>
2022-04-30 14:35:16 +0200

    feat: structure of the about section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navigation from '../Navigation/Navigation';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 const App = () => {
   return (
     <div className="content">
       <Navigation />
       <Header />
+      <Main />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,18 @@
 import './Main.css';
+import aboutImage from '../../images/about_me_image.jpg'
 
 const Main = () => {
 
   return (
-    <main>
-
+    <main className='main'>
+      <section className='about'>
+        <img src={aboutImage} className='about__image'></img>
+        <div className='about__wrapper'>
+          <h2 className='about__title'>About the author</h2>
+          <p className='about__description'>This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.</p>
+          <p className='about__description'>You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.</p>
+        </div>
+      </section>
     </main>
   );
 }
