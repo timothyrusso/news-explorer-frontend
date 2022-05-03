65dd252440b398d7ac8d7bba1705e65a00330584
timothyrusso <russotimothy@live.it>
2022-05-03 08:19:59 +0200

    feat: infotooltip component and navigation links for logged users

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,21 +6,24 @@ import Footer from '../Footer/Footer';
 import SigninPopup from '../SigninPopup/SigninPopup';
 import SignupPopup from '../SignupPopup/SignupPopup';
 import { useState } from "react";
+import InfoTooltip from '../InfoTooltip/InfoTooltip';
 
 const App = () => {
 
   const [isSigninPopupOpen, setIsSigninPopupOpen] = useState(false)
   const [isSignupPopupOpen, setIsSignupPopupOpen] = useState(false)
+  const [isInfoTooltipOpen, setIsInfoTooltipOpen] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const [formValidity, setFormValidity] = useState(true)
   const [errorMessage, setErrorMessage] = useState({})
   const [status, setStatus] = useState("");
-  const [popupRedirectText, setPopupRedirectText] = useState("Sign up");
+  const [popupRedirectText, setPopupRedirectText] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const closeAllPopups = () => {
     setIsSigninPopupOpen(false)
     setIsSignupPopupOpen(false)
-    setPopupRedirectText("Sign up")
+    setIsInfoTooltipOpen(false)
   }
 
   const startLoading = () => {
@@ -39,6 +42,8 @@ const App = () => {
   const handleSigninPopupClick = () => {
     setFormValidity(true)
     setErrorMessage({})
+    closeAllPopups()
+    setPopupRedirectText("Sign up")
     setIsSigninPopupOpen(true)
   }
 
@@ -77,12 +82,13 @@ const App = () => {
 
   return (
     <div className="content">
-      <Navigation onSigninPopupClick={handleSigninPopupClick} />
+      <Navigation onSigninPopupClick={handleSigninPopupClick} loggedIn={loggedIn} />
       <Header />
       <Main />
       <Footer />
       <SigninPopup isOpen={isSigninPopupOpen} onClose={closeAllPopups} onSwitch={handleSwitchPopup} popupRedirectText={popupRedirectText} isLoading={isLoading} startLoading={startLoading} formValidity={formValidity} onFormUpdate={onFormUpdate} errorMessage={errorMessage} onInputUpdate={checkValidity} />
       <SignupPopup isOpen={isSignupPopupOpen} onClose={closeAllPopups} onSwitch={handleSwitchPopup} popupRedirectText={popupRedirectText} isLoading={isLoading} startLoading={startLoading} formValidity={formValidity} onFormUpdate={onFormUpdate} errorMessage={errorMessage} onInputUpdate={checkValidity} />
+      <InfoTooltip isOpen={isInfoTooltipOpen} onClose={closeAllPopups} openSignin={handleSigninPopupClick} />
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,16 @@
 import './Navigation.css';
 import logo from '../../images/NewsExplorer.svg'
+import logout from '../../images/logout.svg'
 
-const Navigation = ({ onSigninPopupClick }) => {
+const Navigation = ({ onSigninPopupClick, loggedIn }) => {
   return (
     <div className="navigation">
       <img src={logo} alt='' className='logo'></img>
       <div className='navigation__wrapper'>
         <a href='' className='navigation__link'>Home</a>
-        <button aria-label="signin" type="button" className='navigation__button' onClick={onSigninPopupClick}>Sign in</button>
+        <a href='' className={`navigation__link navigation__link_type_saved ${!loggedIn ? "navigation__disabled": ""}`}>Saved articles</a>
+        <button aria-label="signin" type="button" className={`navigation__button ${loggedIn ? "navigation__disabled": ""}`} onClick={onSigninPopupClick}>Sign in</button>
+        <button aria-label="signout" type="button" className={`navigation__button navigation__button_type_logout ${!loggedIn ? "navigation__disabled": ""}`} onClick={onSigninPopupClick}>Pippo<img src={logout} alt='' className='navigation__logout-icon'></img></button>
       </div>
     </div>
   );
