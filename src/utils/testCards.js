aeeb94e6989a3095c22c4c3081a58c23a7dc683f
timothyrusso <russotimothy@live.it>
2022-05-12 07:17:12 +0200

    feat: tablet layout for the application

--- src/utils/testCards.js
+++ src/utils/testCards.js
@@ -23,4 +23,20 @@ export const cards = [
     content: 'Suspendisse et vulputate dui, eget pretium lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum dolor est, in vehicula mi suscipit ac. Nam finibus nulla ac faucibus accumsan. Integer facilisis rhoncus orci, vel consequat justo vehicula eget. Donec id posuere libero, eu rutrum orci. Vestibulum odio tellus, rutrum at tincidunt eu, pretium eu mi. Sed lacus justo, eleifend vitae consequat id, tincidunt et tellus. Fusce lacinia placerat metus, non egestas erat. Maecenas quis magna sit amet tortor commodo malesuada a sit amet felis. Maecenas iaculis, odio et tincidunt placerat, arcu neque faucibus nibh, id commodo ante nibh a dui.',
     source: 'National parks traveler',
   },
+  {
+    _id: 2,
+    urlToImage: 'https://www.planetware.com/wpimages/2021/02/italy-best-lakes-lake-como.jpg',
+    date: 'November 4, 2020',
+    title: 'Nature makes you better',
+    content: 'Suspendisse et vulputate dui, eget pretium lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum dolor est, in vehicula mi suscipit ac. Nam finibus nulla ac faucibus accumsan. Integer facilisis rhoncus orci, vel consequat justo vehicula eget. Donec id posuere libero, eu rutrum orci. Vestibulum odio tellus, rutrum at tincidunt eu, pretium eu mi. Sed lacus justo, eleifend vitae consequat id, tincidunt et tellus. Fusce lacinia placerat metus, non egestas erat. Maecenas quis magna sit amet tortor commodo malesuada a sit amet felis. Maecenas iaculis, odio et tincidunt placerat, arcu neque faucibus nibh, id commodo ante nibh a dui.',
+    source: 'national geographic',
+  },
+  {
+    _id: 3,
+    urlToImage: 'https://static.posters.cz/image/750/poster/beach-sunset-i102003.jpg',
+    date: 'November 4, 2020',
+    title: 'Grand Teton Renews Historic Crest Trail',
+    content: 'Suspendisse et vulputate dui, eget pretium lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum dolor est, in vehicula mi suscipit ac. Nam finibus nulla ac faucibus accumsan. Integer facilisis rhoncus orci, vel consequat justo vehicula eget. Donec id posuere libero, eu rutrum orci. Vestibulum odio tellus, rutrum at tincidunt eu, pretium eu mi. Sed lacus justo, eleifend vitae consequat id, tincidunt et tellus. Fusce lacinia placerat metus, non egestas erat. Maecenas quis magna sit amet tortor commodo malesuada a sit amet felis. Maecenas iaculis, odio et tincidunt placerat, arcu neque faucibus nibh, id commodo ante nibh a dui.',
+    source: 'National parks traveler',
+  },
 ];
