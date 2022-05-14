366694c0d53c0bc880facc18f69209127d2238cb
timothyrusso <russotimothy@live.it>
2022-05-14 11:39:01 +0200

    feat: navigation bar overlay effect on mobile

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -50,61 +50,63 @@ const Navigation = ({
   }, []);
 
   return (
-    <div
-      className={`navigation ${navbarColor ? "navigation_type_scroll" : ""}`}
-      style={
-        toggleMenu && screenWidth < 600 ? { backgroundColor: "#1A1B22" } : {}
-      }
-    >
-      <Link to={"/"} className="logo">
-        <img src={blackNavigator ? blackLogo : logo} alt=""></img>
-      </Link>
-      {(toggleMenu || screenWidth > 600) && (
-        <div className="navigation__wrapper">
-          <Link to={"/"} className="navigation__link" style={textColor}>
-            Home
-          </Link>
-          <Link
-            to={"/saved-news"}
-            className={`navigation__link navigation__link_type_saved ${
-              !loggedIn ? "navigation__disabled" : ""
-            }`}
-            style={buttonColor}
-          >
-            Saved articles
-          </Link>
-          <button
-            aria-label="signin"
-            type="button"
-            className={`navigation__button navigation__button_type_signin ${
-              loggedIn ? "navigation__disabled" : ""
-            }`}
-            onClick={onSigninPopupClick}
-            style={buttonColor}
-          >
-            Sign in
-          </button>
-          <button
-            aria-label="signout"
-            type="button"
-            className={`navigation__button navigation__button_type_logout ${
-              !loggedIn ? "navigation__disabled" : ""
-            }`}
-            onClick={handleLogout}
-            style={buttonColor}
-          >
-            TimothyTimothyffffff
-            <img
-              src={blackNavigator ? blackLogout : logout}
-              alt=""
-              className="navigation__logout-icon"
-            ></img>
-          </button>
-        </div>
-      )}
-      <button onClick={toggleNav} className="burgher-button">
-        BTN
-      </button>
+    <div className={toggleMenu && screenWidth < 600 ? "navigation" : ""}>
+      <div
+        className={`navigation__container ${navbarColor ? "navigation__container_type_scroll" : ""}`}
+        style={
+          toggleMenu && screenWidth < 600 ? { backgroundColor: "#1A1B22" } : {}
+        }
+      >
+        <Link to={"/"} className="logo">
+          <img src={blackNavigator ? blackLogo : logo} alt=""></img>
+        </Link>
+        {(toggleMenu || screenWidth > 600) && (
+          <div className="navigation__links-wrapper">
+            <Link to={"/"} className="navigation__link" style={textColor}>
+              Home
+            </Link>
+            <Link
+              to={"/saved-news"}
+              className={`navigation__link navigation__link_type_saved ${
+                !loggedIn ? "navigation__disabled" : ""
+              }`}
+              style={buttonColor}
+            >
+              Saved articles
+            </Link>
+            <button
+              aria-label="signin"
+              type="button"
+              className={`navigation__button navigation__button_type_signin ${
+                loggedIn ? "navigation__disabled" : ""
+              }`}
+              onClick={onSigninPopupClick}
+              style={buttonColor}
+            >
+              Sign in
+            </button>
+            <button
+              aria-label="signout"
+              type="button"
+              className={`navigation__button navigation__button_type_logout ${
+                !loggedIn ? "navigation__disabled" : ""
+              }`}
+              onClick={handleLogout}
+              style={buttonColor}
+            >
+              TimothyTimothyffffff
+              <img
+                src={blackNavigator ? blackLogout : logout}
+                alt=""
+                className="navigation__logout-icon"
+              ></img>
+            </button>
+          </div>
+        )}
+        <button onClick={toggleNav} className="burgher-button">
+          BTN
+        </button>
+      </div>
     </div>
   );
 };
