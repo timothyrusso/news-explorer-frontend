cbf997dc73ea62947a1c8d432fe0d8535c0324f8
Timothy Russo <russotimothy@live.it>
2023-06-06 09:09:47 +0200

    feat: create custom hook to check keywords

---
+++ src/hooks/useCheckKeywords.ts
@@ -0,0 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store/RootState';
+import { setSearchKeywordsListAction } from '../store/user/user.actions';
+
+export const useCheckKeywords = () => {
+  const savedArticles = useSelector(
+    (state: RootState) => state.article.savedArticles
+  );
+
+  const dispatch = useDispatch();
+
+  const checkKeywords = () => {
+    const keywords = savedArticles.map((x) => x.keyword);
+    const count: { [key: string]: number } = {};
+
+    for (let index = 0; index < keywords.length; index++) {
+      const element = keywords[index];
+
+      if (count[element]) {
+        count[element] += 1;
+      } else {
+        count[element] = 1;
+      }
+    }
+
+    const keywordsOrdered = Object.entries(count)
+      .sort((a, b) => b[1] - a[1])
+      .map((element) => element[0]);
+    dispatch(setSearchKeywordsListAction(keywordsOrdered));
+  };
+
+  return { checkKeywords };
+};
