61adf69d8ca0609f75b0219926ed7b5c8482cdd4
Timothy Russo <russotimothy@live.it>
2023-06-16 18:13:48 +0200

    feat: migrate to typescript the main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
-import "./Main.css";
-import aboutImage from "../../images/about_me_image.jpg";
+import React from 'react';
+import aboutImage from '../../images/about_me_image.jpg';
+import './Main.css';
 
 const Main = () => {
   return (
@@ -13,10 +14,11 @@ const Main = () => {
         <div className="about__wrapper">
           <h2 className="about__title">About the author</h2>
           <p className="about__description">
-            Hey there, I'm Timothy. I'm a motivated and passionate 
-            software engineer with a background in eCommerce, Quality Assurance, Application
-            Delivery Management, Project Management and Trade Marketing. I have
-            a bachelor's degree in Economics and Business Management and I have done a coding bootcamp called Practicum.
+            Hey there, I'm Timothy. I'm a motivated and passionate software
+            engineer with a background in eCommerce, Quality Assurance,
+            Application Delivery Management, Project Management and Trade
+            Marketing. I have a bachelor's degree in Economics and Business
+            Management and I have done a coding bootcamp called Practicum.
           </p>
           <p className="about__description">
             I’m a curious person, I love to code and I like to experiment with
