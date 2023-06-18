754d5f614dbf4bb93005239a5129f6641087976f
Timothy Russo <russotimothy@live.it>
2023-06-18 20:00:22 +0200

    feat: migrate to typescript the SavedNews component

--- src/components/SavedNews/SavedNews.tsx
+++ src/components/SavedNews/SavedNews.tsx
@@ -1,14 +1,19 @@
+import React from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 import { useSelector } from 'react-redux';
+import { RootState } from '../../store/RootState';
+import { SavedArticle } from '../../store/article/article.type';
 import './SavedNews.css';
 
 const SavedNews = () => {
-  const savedArticles = useSelector((state) => state.article.savedArticles);
+  const savedArticles = useSelector(
+    (state: RootState) => state.article.savedArticles
+  );
 
   return (
     <section className="saved-news">
       <ul className="search-result-grid">
-        {savedArticles.map((card) => (
+        {savedArticles.map((card: SavedArticle) => (
           <NewsCard key={card._id} card={card} />
         ))}
       </ul>
