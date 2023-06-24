62aec67e54ead690794c39e007f679a08b4f5f25
Timothy Russo <russotimothy@live.it>
2023-06-24 18:32:03 +0200

    feat: migrate to typescript the ProtectedRoute component

--- src/components/ProtectedRoute/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children, ...props }) => {
-  return props.loggedIn ? children : <Navigate to={props.path} />;
-};
-
-export default ProtectedRoute;
---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import React, { FC, ReactNode, ReactElement } from 'react';
+import { Navigate } from 'react-router-dom';
+
+type ProtectedRouteProps = {
+  children: ReactNode;
+  loggedIn?: boolean;
+  path: string;
+};
+
+const ProtectedRoute: FC<ProtectedRouteProps> = ({ children, ...props }) => {
+  return (children &&
+    (props.loggedIn ? (
+      children
+    ) : (
+      <Navigate to={props.path} />
+    ))) as ReactElement | null;
+};
+
+export default ProtectedRoute;
