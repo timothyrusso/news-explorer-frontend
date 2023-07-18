9bd99d432b27c0cd5805af338498d2a950ff1ec0
Timothy Russo <russotimothy@live.it>
2023-07-18 08:48:12 +0200

    feat: accessibility improvements for the NothingFound component

--- src/components/NothingFound/NothingFound.tsx
+++ src/components/NothingFound/NothingFound.tsx
@@ -16,9 +16,9 @@ const NothingFound: FC<NothingFoundProps> = ({
     <div className="nothing-found">
       <img
         src={nothingFoundIcon}
-        alt="Nothing found icon"
+        alt={`No results for ${title}`}
         className="nothing-found__icon"
-      ></img>
+      />
       <h2 className="nothing-found__title">{title}</h2>
       <p className="nothing-found__text">{text}</p>
     </div>
