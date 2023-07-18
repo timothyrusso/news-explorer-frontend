e2d29ef95b41ecedd5f649c45946950cebe6907e
Timothy Russo <russotimothy@live.it>
2023-07-18 16:47:59 +0200

    feat: add new button loader component

---
+++ src/components/ButtonLoader/ButtonLoader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ButtonLoader = () => {
+  return (
+    <div className="button-loader">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ButtonLoader;
