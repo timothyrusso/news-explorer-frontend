c3b7b7d42acb6c6801b97104e1a75e0009ae68ea
Timothy Russo <russotimothy@live.it>
2023-07-20 18:01:35 +0200

    feat: improve accessibility of the Preloader component

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './Preloader.css';
 
-const Preloader = () => {
+type PreloaderProps = {
+  text?: string;
+};
+
+const Preloader: FC<PreloaderProps> = ({ text = 'Searching for news...' }) => {
   return (
-    <div className="preloader">
+    <div className="preloader" aria-live="polite">
       <i className="circle-preloader"></i>
-      <p className="preloader__text">Searching for news...</p>
+      <p className="preloader__text">{text}</p>
     </div>
   );
 };
