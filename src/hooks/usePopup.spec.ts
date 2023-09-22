2636c96388e443664b36f40c780c9abee48726c0
Timothy Russo <russotimothy@live.it>
2023-09-22 08:49:20 +0200

    feat: add test for the usePopup custom hook

---
+++ src/hooks/usePopup.spec.ts
@@ -0,0 +1,86 @@
+import { renderHook, act } from '@testing-library/react';
+import { usePopup } from './usePopup';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  setInfoTooltipClosedAction,
+  setIsSigninPopupClosedAction,
+  setIsSignupPopupClosedAction,
+  setIsSignupPopupOpenAction,
+} from '../store/toggles/toggles.actions';
+import {
+  removeErrorMessageAction,
+  setIsFormValidityTrueAction,
+  setPopupserverErrorMessageAction,
+} from '../store/errors/errors.actions';
+
+jest.mock('react-redux', () => ({
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
+
+describe('usePopup', () => {
+  let mockDispatch: jest.Mock;
+  const mockedUseDispatch = useDispatch as jest.Mock;
+  const mockedUseSelector = useSelector as jest.Mock;
+
+  beforeEach(() => {
+    mockDispatch = jest.fn();
+    mockedUseDispatch.mockReturnValue(mockDispatch);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should close all popups', () => {
+    const { result } = renderHook(() => usePopup());
+
+    act(() => {
+      result.current.closeAllPopups();
+    });
+
+    expect(mockDispatch).toHaveBeenCalledWith(setIsSigninPopupClosedAction());
+    expect(mockDispatch).toHaveBeenCalledWith(setIsSignupPopupClosedAction());
+    expect(mockDispatch).toHaveBeenCalledWith(setInfoTooltipClosedAction());
+    expect(mockDispatch).toHaveBeenCalledWith(
+      setPopupserverErrorMessageAction('')
+    );
+  });
+
+  it('should handle signin popup click', () => {
+    const { result } = renderHook(() => usePopup());
+
+    act(() => {
+      result.current.handleSigninPopupClick();
+    });
+
+    expect(mockDispatch).toHaveBeenCalledWith(setIsFormValidityTrueAction());
+    expect(mockDispatch).toHaveBeenCalledWith(removeErrorMessageAction());
+  });
+
+  it('should handle switch popup', () => {
+    mockedUseSelector.mockReturnValueOnce(true).mockReturnValueOnce(false);
+    const { result } = renderHook(() => usePopup());
+
+    act(() => {
+      result.current.handleSwitchPopup();
+    });
+
+    expect(mockDispatch).toHaveBeenCalledWith(setIsSigninPopupClosedAction());
+    expect(mockDispatch).toHaveBeenCalledWith(setIsSignupPopupOpenAction());
+  });
+
+  it('should open popup if not logged in', () => {
+    localStorage.removeItem('jwt');
+    const { result } = renderHook(() => usePopup());
+
+    const mockHandleSigninPopupClick = jest.fn();
+    result.current.handleSigninPopupClick = mockHandleSigninPopupClick;
+
+    act(() => {
+      result.current.openPopupIfNotLoggedin();
+    });
+
+    expect(mockHandleSigninPopupClick).toHaveBeenCalled();
+  });
+});
