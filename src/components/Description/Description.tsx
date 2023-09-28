a098d04b45f5ec8262c2c2c74b6eac9b5e5189c0
Timothy Russo <russotimothy@live.it>
2023-09-28 09:38:13 +0200

    refactor: improve Main component

---
+++ src/components/Description/Description.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Description.css';
+
+const Description = () => {
+  return (
+    <article className="description">
+      <h2 className="description__title">About the author</h2>
+      <p className="description__text">
+        Hey there, I'm Timothy. I'm a motivated and passionate frontend engineer
+        with a background in eCommerce, Quality Assurance, Application Delivery
+        Management, Project Management and Trade Marketing. I have a bachelor's
+        degree in Economics and Business Management and I have done a coding
+        bootcamp called TripleTen.
+      </p>
+      <p className="description__text">
+        I’m a curious person, I love to code and I like to experiment with new
+        technologies. Now coding is my passion and my professional career focus.
+      </p>
+    </article>
+  );
+};
+
+export default Description;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import aboutImage from '../../images/about_me_image.jpg';
+import Description from '../Description/Description';
 import './Main.css';
 
 const Main = () => {
@@ -11,21 +12,7 @@ const Main = () => {
           className="about__image"
           alt="Timothy - a passionate frontend engineer"
         ></img>
-        <article className="about__wrapper">
-          <h2 className="about__title">About the author</h2>
-          <p className="about__description">
-            Hey there, I'm Timothy. I'm a motivated and passionate frontend
-            engineer with a background in eCommerce, Quality Assurance,
-            Application Delivery Management, Project Management and Trade
-            Marketing. I have a bachelor's degree in Economics and Business
-            Management and I have done a coding bootcamp called TripleTen.
-          </p>
-          <p className="about__description">
-            I’m a curious person, I love to code and I like to experiment with
-            new technologies. Now coding is my passion and my professional
-            career focus.
-          </p>
-        </article>
+        <Description />
       </section>
     </main>
   );
